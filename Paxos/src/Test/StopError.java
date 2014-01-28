aa03856569feff8242e440513ca8895df87e7677
harishbsingh <harish.b.singh@gmail.com>
2014-01-27 22:03:39 -0800

    Update StopError.java

--- Paxos/src/Test/StopError.java
+++ Paxos/src/Test/StopError.java
@@ -4,12 +4,12 @@ public class StopError extends Error {
 	
 	StopError(String errorMessage)
 	{
-		
+		//toDo:logs the error message.
 	}
 	
 	public void stopThread()
 	{
-		
+		//toDo:code to stop the thread
 	}
 	
 
